aeb6b2225b7538f9659e44383934b688a8a1c918
Vi Pc <vicniclok@gmail.com>
2024-04-28 10:55:22 -0500

    Creating the ship, and centering it to the screen's midbottom.

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ class Settings:
         #Screen settings
         self.screen_width = 1600
         self.screen_height = 800
-        self.bg_color = (230, 230, 100)
+        self.bg_color = (230, 230, 230)
---
+++ ship.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Ship:
+    """A class for the ship in the game"""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and setting its starting position"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        #Loading the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        #Putting ship at bottom center of screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Drawing the ship"""
+        self.screen.blit(self.image, self.rect)

a7da8c2b63e3b48f1be9568471054fdf3dd781d1
Vi Pc <vicniclok@gmail.com>
2024-04-27 21:44:16 -0500

    Added pygame .Clock to control frames.

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources"""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1600, 800))
         pygame.display.set_caption("Alien Invasion")
@@ -22,6 +23,7 @@ class AlienInvasion:
 
             #displaying the drawn screen
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     #Instance of game, and running it

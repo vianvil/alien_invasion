03f93f26cbfb0fc0d660f8be1a23651dd59bb44e
Vi Pc <vicniclok@gmail.com>
2024-04-28 11:44:01 -0500

    refactoring main game loop, clean up code.

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,19 +24,29 @@ class AlienInvasion:
         """Start the main loop for the game (How the game will run, and how to exit)"""
         while True:
             #Monitor keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            #Redrawing the screen each pass
-            self.screen.fill(self.settings.bg_color)
-
-            #Drawing the ship in the game
-            self.ship.blitme()
-            
-            #displaying the drawn screen
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """responds to events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    sys.exit()
+
+    def _update_screen(self):
+         """monitor the screen and update it
+            all these codes have to do with screen updating and drawing"""
+         
+         #redrawing the screen during each call of the loop
+         self.screen.fill(self.settings.bg_color)
+        
+         #draws the ship in the game
+         self.ship.blitme()
+
+         #displaying the screen
+         pygame.display.flip()
+
 
 if __name__ == '__main__':
     #Instance of game, and running it
